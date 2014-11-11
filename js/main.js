a32a6e485fa605b38dee41f667e03269d870bc4d
unknown <jacquelinecurrie@gmail.com>
2014-11-11 20:22:40 +0000

    added new questions

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ app.controller("questions",function($scope, $http) {
   $scope.hasSubmitted = false;
 
   $scope.answers = {
-    hear: "", friend: "", offer: " ", comments: ""
+    times: "", hear: "", favourite: "", improve: "", likely: "", comments: ""
   }
 
   $scope.submit = function() {
@@ -16,41 +16,6 @@ app.controller("questions",function($scope, $http) {
     xhr.setRequestHeader('Content-Type', 'text/plain');
     xhr.send(JSON.stringify({data: $scope.answers}));
     $scope.hasSubmitted = true;
-    /*success(function(data, status, headers, config) {
-        $scope.hasSubmitted = true;
-      // this callback will be called asynchronously
-      // when the response is available
-    }).
-    error(function(data, status, headers, config) {
-      // called asynchronously if an error occurs
-      // or server returns response with an error status.
-    });*/
   }
 });
 
-
-// angular.injector(["ng"]).invoke(function($http) {
-
-//   var err = console.error.bind(console);
-//   var yay = console.log.bind(console);
-
-//   // retrieve all
-//   $http.get("/api/things")
-//     .then(yay,err)
-
-//   // complete CRUD example
-//   $http.post("/api/things",{name: "hi"})
-//     .then(function(resp) {
-//       return $http.put("/api/things/" + resp.data.id,{name: "changed"})
-//     })
-//     .then(function(resp) {
-//       return $http.get("/api/things/" + resp.data.id).then(function(resp) {
-//         if(resp.data.name !== "changed") throw new Error("Couldn't update/retrieve")
-//         return resp
-//       })
-//     })
-//     .then(function(resp) {
-//       return $http.delete("/api/things/" + resp.data.id)
-//     },err)
-
-// });
